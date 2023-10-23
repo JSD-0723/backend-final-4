d2fbd20a79a87bf14464e9c28eb5bfd79bbb6d6d
Hanna-Hinn <hanna.hinn30@gmail.com>
2023-10-23 14:34:04 +0300

    Update brandSeeder.js

--- utils/constants/brandSeeder.js
+++ utils/constants/brandSeeder.js
@@ -1,27 +1,27 @@
 module.exports = [
   {
     id: 1,
-    name: 'Brand 1',
+    name: 'Zara',
     imageUrl: 'https://example.com/brand1.jpg',
   },
   {
     id: 2,
-    name: 'Brand 2',
+    name: 'Dolce & Gabbana',
     imageUrl: 'https://example.com/brand2.jpg',
   },
   {
     id: 3,
-    name: 'Brand 3',
+    name: 'H&M',
     imageUrl: 'https://example.com/brand3.jpg',
   },
   {
     id: 4,
-    name: 'Brand 4',
+    name: 'Chanel',
     imageUrl: 'https://example.com/brand4.jpg',
   },
   {
     id: 5,
-    name: 'Brand 5',
+    name: 'Prada',
     imageUrl: 'https://example.com/brand5.jpg',
   },
 ];
