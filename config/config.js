6e18236fef03422d939e6fb1cf4e867997f96f8f
Atallah <Atallah.192000@gmail.com>
2023-10-28 23:11:00 +0300

    testing seeders for docker

--- config/config.js
+++ config/config.js
@@ -2,14 +2,14 @@ const process = require('process');
 require('dotenv').config();
 
 module.exports = {
-   development: {
+  development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'mysql',
     jwt_secret: process.env.JWT_SECRET,
-    logging: console.log,
+    // logging: false,
     deliveryFee: 12,
     itemsPerPage: 20,
     newArrivalMonths: 3,
