8499b668184271484a07399075c1a8434556f399
Hanna-Hinn <hanna.hinn30@gmail.com>
2023-11-10 22:08:54 +0200

    Update wishListItemModel.js

--- models/wishListItemModel.js
+++ models/wishListItemModel.js
@@ -4,41 +4,45 @@ const sequelize = require('../utils/dataBaseConnection');
 const WishList = require('./wishListModel');
 const Product = require('./productModel');
 
-const WishListItem = sequelize.define('wishListItem', {
+const WishListItem = sequelize.define(
+  'wishListItem',
+  {
     id: {
-        type: DataTypes.INTEGER(15),
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-    }, wishListId: {
-        type: DataTypes.INTEGER(15),
-        allowNull: false,
-        references: {
-            model: WishList,
-            key: 'id',
-            onDelete: 'CASCADE',
-        },
+      type: DataTypes.INTEGER(15),
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    wishListId: {
+      type: DataTypes.INTEGER(15),
+      allowNull: false,
+      references: {
+        model: WishList,
+        key: 'id',
+        onDelete: 'CASCADE',
+      },
     },
     productId: {
-        type: DataTypes.INTEGER(15),
-        allowNull: true,
-        references: {
-            model: Product,
-            key: 'id',
-            onDelete: 'CASCADE',
-        },
+      type: DataTypes.INTEGER(15),
+      allowNull: true,
+      references: {
+        model: Product,
+        key: 'id',
+        onDelete: 'CASCADE',
+      },
     },
-}, {
+  },
+  {
     freezeTableName: true,
     timestamps: false,
-});
+  }
+);
 
 // Associations
-WishListItem.belongsTo(WishList, { onDelete: 'cascade', hooks: true }, { foreignKey: 'wishListId' });
+WishListItem.belongsTo(WishList, { foreignKey: 'wishListId' });
 WishList.hasMany(WishListItem, { foreignKey: 'wishListId' });
 
-WishListItem.belongsTo(Product, /*{ onDelete: 'cascade', hooks: true } ,*/ { foreignKey: 'productId' });
+WishListItem.belongsTo(Product, { foreignKey: 'productId' });
 Product.hasMany(WishListItem, { foreignKey: 'productId' });
-
 
 module.exports = WishListItem;
